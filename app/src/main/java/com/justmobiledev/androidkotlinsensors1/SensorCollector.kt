2c4152406ec5a141d256aecef9db76a70c03f7fc
justmobiledev <justmobiledev1@gmail.com>
2019-02-07 06:40:22 -0500

    adding project files

--- app/src/main/java/com/justmobiledev/androidkotlinsensors1/SensorCollector.kt
+++ app/src/main/java/com/justmobiledev/androidkotlinsensors1/SensorCollector.kt
@@ -1,2 +1,16 @@
 package com.justmobiledev.androidkotlinsensors1
 
+import android.content.Context
+
+object SensorCollector{
+
+    lateinit var context : Context
+
+    init{
+
+    }
+
+    fun addContext(ctx : Context){
+        context = ctx
+    }
+}
