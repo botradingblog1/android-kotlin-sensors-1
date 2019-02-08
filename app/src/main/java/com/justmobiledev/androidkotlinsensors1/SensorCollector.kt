c00a3507525996b1d986d4e1bd79d0a120be111a
justmobiledev <justmobiledev1@gmail.com>
2019-02-08 06:45:53 -0500

    adding project files

--- app/src/main/java/com/justmobiledev/androidkotlinsensors1/SensorCollector.kt
+++ app/src/main/java/com/justmobiledev/androidkotlinsensors1/SensorCollector.kt
@@ -1,16 +1,21 @@
 package com.justmobiledev.androidkotlinsensors1
 
 import android.content.Context
+import android.hardware.SensorManager
+import android.support.v4.content.ContextCompat.getSystemService
 
 object SensorCollector{
 
     lateinit var context : Context
+    var mySensorManager : SensorManager
 
     init{
-
+        mySensorManager = getSystemService(cntext, Context.SENSOR_SERVICE) as SensorManager;
     }
 
     fun addContext(ctx : Context){
         context = ctx
     }
+
+
 }
