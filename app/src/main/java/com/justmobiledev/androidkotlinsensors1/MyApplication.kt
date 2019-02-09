81eef3c7df979e30f3afcce8041108d356908dcb
justmobiledev <justmobiledev1@gmail.com>
2019-02-09 03:45:33 -0500

    adding project files

---
+++ app/src/main/java/com/justmobiledev/androidkotlinsensors1/MyApplication.kt
@@ -0,0 +1,15 @@
+package com.justmobiledev.androidkotlinsensors1
+
+import android.app.Application
+import android.content.Context
+
+class MyApplication : Application() {
+
+    companion object {
+        fun getContext(): Context{
+            return this.getContext()
+        }
+    }
+
+
+}
--- app/src/main/java/com/justmobiledev/androidkotlinsensors1/SensorCollector.kt
+++ app/src/main/java/com/justmobiledev/androidkotlinsensors1/SensorCollector.kt
@@ -2,22 +2,40 @@ package com.justmobiledev.androidkotlinsensors1
 
 import android.app.Service
 import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
 import android.hardware.SensorManager
+import android.util.Log
 
 
-object SensorCollector{
+object SensorCollector: SensorEventListener {
+    private val TAG : String = "SensorCollector"
 
-    lateinit var context : Context
-    var mySensorManager : SensorManager
+    private var sensorManager : SensorManager ?= null
+    private var accelerometer : Sensor ?= null
+    private var context : Context ?= null
 
     init{
-        mySensorManager = (context.getSystemService(Service.SENSOR_SERVICE)) as SensorManager
-
+        sensorManager = (MyApplication.getContext().getSystemService(Service.SENSOR_SERVICE)) as SensorManager
     }
 
-    fun addContext(ctx : Context){
-        context = ctx
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+        Log.d(TAG, ""+accuracy)
     }
 
+    override fun onSensorChanged(event: SensorEvent?) {
+        if (event != null) {
+            // ground!!.updateMe(event.values[1] , event.values[0])
+            Log.d(TAG, ""+event.values[1]+", "+event.values[0])
+        }
+    }
 
+    fun start(){
+        sensorManager!!.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_GAME)
+    }
+
+    fun stop(){
+        sensorManager!!.unregisterListener(this)
+    }
 }
